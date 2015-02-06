10ca3d22a49b214ca82a5a996836fb54b321c82c
zhenhou <zhn.hou@gmail.com>
2015-02-06 17:22:45 -0600

    new signal sims

--- planck_sims/signal_sims/scripts/submit_143_signal.sh
+++ planck_sims/signal_sims/scripts/submit_143_signal.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=143_signal
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 
-cd /home/zhenhou/Projects/projects/spt_x_planck/planck_halfring_sims/signal_sims
+cd /home/zhenhou/Projects/projects/spt_x_planck/planck_sims/signal_sims
 
 mpirun -np 4 ./sxp_sims ini_zone/hfi_143_signal.ini
