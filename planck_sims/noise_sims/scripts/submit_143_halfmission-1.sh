820cc5dce0be344bda9791afaa7ee61460c6459f
zhenhou <zhn.hou@gmail.com>
2015-02-07 11:44:55 -0600

    noise sims submit

---
+++ planck_sims/noise_sims/scripts/submit_143_halfmission-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=kicp
+#SBATCH --account=kicp
+#SBATCH --nodes=4
+#SBATCH --exclusive
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=143_mh1
+#SBATCH --time=12:00:00
+
+cd /home/zhenhou/Projects/projects/spt_x_planck/planck_sims/noise_sims
+
+mpirun -np 4 ./sxp_sims ini_zone/hfi_143_ringhalf_1.ini
